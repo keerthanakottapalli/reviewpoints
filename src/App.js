0269897cc99db950af1b0bc4cfaa8f02890b0942
keerthanakottapalli <123591002+keerthanakottapalli@users.noreply.github.com>
2023-11-30 00:59:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import RegistrationForm from './Registration';
 function App() {
   return (
     <div className="App">
-      
+       <Router>
+        <Routes>
+        <Route path='/registration' element={< RegistrationForm/>} />
+        </Routes>
+       </Router>
     </div>
   );
 }
