6c692e27d8ea216960573c99d9305a0f7498f4fd
keerthanakottapalli <123591002+keerthanakottapalli@users.noreply.github.com>
2023-11-30 17:37:57 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import RegistrationForm from './Registration';
+import LoginForm from './Login';
 function App() {
   return (
     <div className="App">
        <Router>
         <Routes>
         <Route path='/registration' element={< RegistrationForm/>} />
+        <Route path='/loginform' element={< LoginForm/>} />
         </Routes>
        </Router>
     </div>
